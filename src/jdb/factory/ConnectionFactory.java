b3ce4ed5ad9c347847152b72975a47cbf12b9e3e
Mario Wilson Garcia <70682960+gwm1965@users.noreply.github.com>
2023-10-03 05:35:05 -0300

    projeto completo

---
+++ src/jdb/factory/ConnectionFactory.java
@@ -0,0 +1,32 @@
+package jdb.factory;
+
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+
+    public DataSource dataSource;
+
+    public ConnectionFactory() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/hotelalura?useTimezone=true&serverTimezone=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("Alura#1900");
+
+        this.dataSource = comboPooledDataSource;
+    }
+
+    public Connection recuperarConexao() {
+        try {
+            return this.dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
